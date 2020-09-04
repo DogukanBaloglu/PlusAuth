03025043310b347349c3138cc91987bdea9f3443
DogukanBaloglu <48068925+DogukanBaloglu@users.noreply.github.com>
2020-09-04 17:07:44 +0300

    v1

--- plusauth_rest_py/plusauth.py
+++ plusauth_rest_py/plusauth.py
@@ -1,12 +1,12 @@
-from plusauth_rest_py.apis import ApiService
-from plusauth_rest_py.clients import ClientService
-from plusauth_rest_py.connections import ConnectionService
-from plusauth_rest_py.permissions import PermissionServise
-from plusauth_rest_py.rolegroups import RoleGroupsService
-from plusauth_rest_py.roles import RoleService
-from plusauth_rest_py.tenants import TenantsService
-from plusauth_rest_py.users import UsersService
-from plusauth_rest_py.views import ViewsService
+from .apis import ApiService
+from .clients import ClientService
+from .connections import ConnectionService
+from .permissions import PermissionServise
+from .rolegroups import RoleGroupsService
+from .roles import RoleService
+from .tenants import TenantsService
+from .users import UsersService
+from .views import ViewsService
 
 class Auth0():
     """
